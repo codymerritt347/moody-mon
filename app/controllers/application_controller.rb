3130b0ddc37a65299a380c9fc49b5bf180a897ef
codymerritt347 <cody.merritt@icloud.com>
2021-02-10 16:15:41 -0500

    Fill out basic routes in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/error' do
+    erb :error
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
-  get '/users/new'
-    erb :
+  get '/users' do
+    erb :'/users/index'
+  end
+
+  get '/users/new' do
+    erb :'/users/new'
+  end
 
 end
