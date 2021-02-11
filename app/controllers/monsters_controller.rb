dd6b66d12be2ec820ac437d7e20d57ab5ceae5fe
codymerritt347 <cody.merritt@icloud.com>
2021-02-10 22:46:26 -0500

    Add five monsters' strong and weak images to public

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,2 +1,4 @@
 class MonstersController < ApplicationController
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,8 @@ class UsersController < ApplicationController
     erb :'/users/new'
   end
 
+  get '/users/:id' do
+    erb :'/users/show'
+  end
+
 end
