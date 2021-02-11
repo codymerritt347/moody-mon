5c2bca1c8dfc35c7dbe211c6b6cd27da2d59e667
codymerritt347 <cody.merritt@icloud.com>
2021-02-11 14:15:09 -0500

    Add foreign keys to seeded models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'faker'
 
+counter = 0
 10.times do
+  counter += 1
   User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -14,14 +16,17 @@ require 'faker'
     level: 0,
     current_health: 10,
     max_health: 10,
+    user_id: counter
     )
   Entry.create(
     title: Faker::Lorem.sentence(word_count: 3),
     note: Faker::Lorem.sentences(number: 3),
+    user_id: counter
   )
   Mood.create(
     name: Faker::Color.color_name,
     effect: ["up", "down"].sample,
     amount: Faker::Number.between(from: 1, to: 3),
+    entry_id: counter
   )
 end
