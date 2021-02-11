56f27606b30ad608cf66644c8878c52eb2d1f5eb
codymerritt347 <cody.merritt@icloud.com>
2021-02-11 16:51:45 -0500

    Add validations to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  has_secure_password #password, password=, authenticate, pw_confirmation, ...
   has_one :monster
   has_many :entries
   has_many :moods, through: :entries
+
+  has_secure_password
+  validates :first_name, prescence: true
+  validates :last_name, prescence: true
+  validates :email, prescence: true, uniqueness: true, format: {width: //}
+  validates :password, length: {in: 10..100}, confirmation: true
+  validates :hometown, prescence: true
+  validates :favorite_food, prescence: true
 end
