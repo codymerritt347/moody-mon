5f270596cb4569cce120856bf3dbf1307e8c292f
codymerritt347 <cody.merritt@icloud.com>
2021-02-11 17:04:05 -0500

    Improve user email validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, prescence: true
   validates :last_name, prescence: true
-  validates :email, prescence: true, uniqueness: true, format: {width: //}
+  validates :email, prescence: true, uniqueness: true, format: {width: /(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(<domain>[a-z]{2,}))/i}
   validates :password, length: {in: 10..100}, confirmation: true
   validates :hometown, prescence: true
   validates :favorite_food, prescence: true
