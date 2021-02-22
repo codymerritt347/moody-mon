ad0d57dd8289078b07b3870a9df07a04d3d638ae
codymerritt347 <cody.merritt@icloud.com>
2021-02-21 23:57:49 -0500

    Add color background to Monster avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   get '/users/signup' do
+
     erb :"/users/signup"
   end
 
@@ -9,32 +10,39 @@ class UsersController < ApplicationController
     if @user.valid?
       @user.save
       session[:user_id] = @user.id
+    
       redirect '/monsters/new'
     else
+
       erb :error
     end
   end
 
   get '/users/:id' do
     @user = User.find(params[:id])
+
     erb :"/users/show"
   end
 
   get '/users/:id/edit' do
     @user = User.find(params[:id])
+
     erb :"/users/edit"
   end
 
   patch '/users/:id' do
+
     redirect '/users/:id'
   end
 
   delete '/users/:id/delete' do
+
     @user = User.find(params[:id])
     redirect '/users'
   end
 
   helpers do
+    
     def self.current_user(session)
       @user = User.find_by_id(session[:user_id])
     end
