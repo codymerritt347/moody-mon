d5b88dcf81a87cacc911a06db67a4c7a5fdab656
codymerritt347 <cody.merritt@icloud.com>
2021-02-22 19:07:10 -0500

    Create migrations for new coins attribute in users and new image attribute for Monsters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_020156) do
+ActiveRecord::Schema.define(version: 2021_02_23_000234) do
 
   create_table "entries", force: :cascade do |t|
     t.string "time_of_day"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_020156) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_monsters_on_user_id"
   end
 
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_020156) do
     t.integer "streak"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "coins"
   end
 
 end
