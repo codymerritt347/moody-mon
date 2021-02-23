955af890145d14470504af20f096cffee76bb145
codymerritt347 <cody.merritt@icloud.com>
2021-02-23 14:10:19 -0500

    Add validations to User model for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  has_one :monster
-  has_many :entries
   has_secure_password
+  validates :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+
+  has_one :monster, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
